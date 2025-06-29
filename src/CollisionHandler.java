ecb176a879ad48debea1d506e644902d67831033
V-Q-M <vr-mueller@protonmail.com>
2025-06-29 14:12:31 +0000

    Implemented border collision

--- src/CollisionHandler.java
+++ src/CollisionHandler.java
@@ -6,7 +6,20 @@ public class CollisionHandler {
     }
 
     boolean playerCollision(int playerX, int playerY, int playerWidth, int playerHeight, int speed, String direction){
-
+        switch (direction){
+            case "up" -> {
+                return (playerY - speed <= 0);
+            }
+            case "down" -> {
+                return (playerY + playerHeight + speed >= Main.HEIGHT);
+            }
+            case "left" -> {
+                return (playerX - speed <= 0);
+            }
+            case "right" -> {
+                return (playerX + playerWidth + speed >= Main.WIDTH);
+            }
+        }
 
         return false;
     }
